2a111d077f303568a1e649fcdd15995610f5da6d
ShongSu <chinhouu1992@gmail.com>
2018-01-31 16:07:27 -0500

    change android localnotification from default_all to VIBRATE only

--- android/src/main/java/com/wix/reactnativenotifications/core/notifications/LocalNotification.java
+++ android/src/main/java/com/wix/reactnativenotifications/core/notifications/LocalNotification.java
@@ -138,7 +138,7 @@ public class LocalNotification implements ILocalNotification {
                 .setSmallIcon(icon != null ? icon : mContext.getApplicationContext().getApplicationInfo().icon)
                 .setSound(mNotificationProps.getSound())
                 .setContentIntent(intent)
-                .setDefaults(Notification.DEFAULT_ALL)
+                .setDefaults(Notification.DEFAULT_VIBRATE)
                 .setAutoCancel(true);
 
         final Integer color = mNotificationProps.getColor();
