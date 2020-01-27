03811399e23901fcf81d5e931cf58ed210de3e16
Chandler Newman-Reed <chandler.newmanreed@aetonixsystems.com>
2020-01-27 15:02:57 -0500

    Remove everything

--- android/src/main/java/com/wix/reactnativenotifications/core/notificationdrawer/INotificationDrawerApplication.java
+++
@@ -1,9 +0,0 @@
-package com.wix.reactnativenotifications.core.notificationdrawer;
-
-import android.content.Context;
-
-import com.wix.reactnativenotifications.core.AppLaunchHelper;
-
-public interface INotificationDrawerApplication {
-    INotificationDrawer getNotificationsDrawer(Context context, AppLaunchHelper defaultAppLaunchHelper);
-}
